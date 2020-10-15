67bf003f7ae4789f780caafba92585ddc4fd0573
Vasco Franco <Vasco-jofra@users.noreply.github.com>
2020-10-15 16:29:27 +0100

    more content on ps arg article

--- assets/article_tests/2020-10-06-3_ps_arg/test.c
+++ assets/article_tests/2020-10-06-3_ps_arg/test.c
@@ -2,6 +2,19 @@
 #include <string.h>
 #include <unistd.h>
 
+void get_comm_name(char *buf, size_t buf_sz) {
+    FILE *f = fopen("/proc/self/comm", "r");
+    memset(buf, 0, buf_sz);
+    fread(buf, buf_sz, 1, f);
+    fclose(f);
+}
+
+void set_comm_name(char *buf, size_t buf_sz) {
+    FILE *f = fopen("/proc/self/comm", "w");
+    fwrite(buf, buf_sz, 1, f);
+    fclose(f);
+}
+
 int main(int argc, char *argv[]) {
     if(argc != 3) {
         printf("I want 3 arguments");
@@ -12,6 +25,17 @@ int main(int argc, char *argv[]) {
     memcpy(argv[1], "xxxxxxxxxxxxxxxx", strlen(argv[1]));
     memcpy(argv[2], "yyyyyyyyyyyyyyyy", strlen(argv[2]));
 
+    char buf[64];
+    get_comm_name(buf, sizeof(buf));
+    printf("old_comm = %s", buf);
+
+    set_comm_name("hidden_prog", 11);
+
+    get_comm_name(buf, sizeof(buf));
+    printf("new_comm = %s", buf);
+
     sleep(1000);
+
+
     return 0;
 }
